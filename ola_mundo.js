f5001bf583b71f56b008a611b657cd46bbbde90d
Maxok System <42083051+alexisjunkes@users.noreply.github.com>
2021-11-16 20:31:24 -0300

    aa

---
+++ ola_mundo.js
@@ -0,0 +1,19 @@
+
+
+
+//Abre uma caixa de alerta simples
+//window.alert("Minha primeira mensagem")
+//abre uma caixa de sim e não
+//window.confirm("Esta gostando de JS?")
+//abre um caixa para digitar algo
+//window.prompt("Qual é teu nome?")
+
+
+
+/*
+as variaveis em JS podem trocar de tipo sem metodos para isso 
+*/
+
+var variavel = "alexis"
+typeof variavel
+//ele retorna o tipo da variavel
